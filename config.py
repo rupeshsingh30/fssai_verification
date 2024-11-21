82023846503498974f7c9bafe076970c933809ee
Rupesh <rupeshsingh.choudhary@sequelstring.com>
2024-11-21 14:54:42 +0530

    Remove sensitive key

--- config.py
+++ config.py
@@ -20,4 +20,3 @@ input_excel = r'D:\Fssai\codes\Sample List Merchants & FSSAI.xlsx'
 captcha_img = r'D:\Fssai\codes\ss_image.jpg'
 
 
-url_for_captcha = "https://sequelpythonapps.azurewebsites.net/api/captcha/d?code=mu3llrtj1HD2J4GjFvW3f5Tcgnv-cNEXVOKsDOOp0AzXAzFuvXOuYQ=="
